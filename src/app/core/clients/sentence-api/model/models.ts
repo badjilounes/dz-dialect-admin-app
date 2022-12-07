7572962bf200ce50dab06ac592cb5b31b2537f8a
Ghilès <ghiles.badji@gmail.com>
2022-12-07 16:31:10 +0100

    feat: openapi setup

---
+++ src/app/core/clients/sentence-api/model/models.ts
@@ -0,0 +1,6 @@
+export * from './bulk-create-sentence-dto';
+export * from './bulk-sentence-additional-information-dto';
+export * from './bulk-sentence-dto';
+export * from './bulk-sentence-word-proposition-dto';
+export * from './create-sentence-dto';
+export * from './sentence-response-dto';
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { environment } from 'src/environments/environment';
+import { Configuration, ConfigurationParameters, ApiModule } from './clients/sentence-api';
+import { HttpClientModule } from '@angular/common/http';
+
+function apiConfigFactory(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: environment.apiUrl,
+  };
+  return new Configuration(params);
+}
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, ApiModule.forRoot(apiConfigFactory), HttpClientModule
+  ]
+})
+export class CoreModule { }
+
+
